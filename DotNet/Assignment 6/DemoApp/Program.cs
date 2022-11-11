2d3210a11bc382d456b8256f3d56b9d4306c31c0
Suraj Ulhas Gate <114273517+surajgate@users.noreply.github.com>
2022-11-11 12:41:36 +0530

    Add files via upload

---
+++ DotNet/Assignment 6/DemoApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+delegate double TourRate(int days, int Noperson);
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int d = int.Parse(args[0]);
+        int p = int.Parse(args[1]);
+        Type t = Type.GetType(args[2]);
+        MethodInfo scheme = t.GetMethod(args[3]);
+        object package = Activator.CreateInstance(t);
+        TourRate r = scheme.CreateDelegate<TourRate>(package);
+
+        Console.WriteLine($"Total Tour rate:{r(d, p)}");
+    }
+
+
+}
---
+++ DotNet/Assignment 6/TourLib/EconomyTours.cs
@@ -0,0 +1,7 @@
+﻿namespace Tours;
+
+public class EconomyTours 
+{
+    public double Common(int days, int Noperson) => days * Noperson * 500;
+    
+}
